978fd0297971b6aca689ece2d907317cea98cbc4
Robert - Andrei Bogdan <arobert.bogdan@gmail.com>
2015-05-20 19:19:45 +0300

    Added posts search functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all
+    if params[:search] != nil
+    	like_keyword = "%#{params[:search]}%"
+    	@posts = Post.where('description LIKE ? OR title LIKE ?',like_keyword, like_keyword)
+	end
   end
 end
