c672b6fd6683c4f3f63293c88d8514d5c1aff76e
SmookyGG <arobert.bogdan@gmail.com>
2015-05-27 16:30:59 +0300

    change folloing user profile picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,17 @@ Category.create(name: "news")
 Category.create(name: "sports")
 
 user = User.create! :nickname => 'user1', :email => 'user1@gmail.com', :password => '1q2w3e4r5t', :password_confirmation => '1q2w3e4r5t'
+user2 = User.create! :nickname => 'user2', :email => 'user2@gmail.com', :password => '1q2w3e4r5t', :password_confirmation => '1q2w3e4r5t'
+
+Follow.create(:user_id => user.id, :follow_id => user2.id)
 
 categories = Category.all
 
 categories.each{ |category|
-  20.times{
-    p = Post.create(title: "Post"+rand(200).to_s+category.name, description:"test", category_id: category.id, media: "www.youtube.com", user_id: user.id, rating: rand(4000) )
+  5.times{
+    p = Post.create(title: "Post"+rand(200).to_s+category.name, description:"test", category_id: category.id, media: "http://www.youtube.com", user_id: user.id, rating: rand(4000)-2000 )
     begin
-      object = ::LinkThumbnailer.generate('http://' + p.media)
+      object = ::LinkThumbnailer.generate(p.media)
       p.thumbnail = object.images.first.src
       p.save
     rescue Net::HTTP::Persistent::Error
